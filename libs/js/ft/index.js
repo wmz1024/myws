a2a0ab78cb310949837aa594683fa738c8eed646
wmz1024 <wmz872311@163.com>
2021-04-12 18:41:14 +0800

    11

--- libs/js/ft/index.js
+++ libs/js/ft/index.js
@@ -2,12 +2,12 @@ var OriginTitle = document.title;
  var titleTime;
  document.addEventListener('visibilitychange', function () {
      if (document.hidden) {
-         $('[rel="icon"]').attr('href', "https://cdn.jsdelivr.net/gh/wmz1024/wmz1024.github.io/favicon.ico");
+         $('[rel="icon"]').attr('href', "https://blog.wmz.link/favicon.ico");
          document.title = '我相信你还会回来的！' + OriginTitle;
          clearTimeout(titleTime);
      }
      else {
-         $('[rel="icon"]').attr('href', "https://cdn.jsdelivr.net/gh/wmz1024/wmz1024.github.io/favicon.ico");
+         $('[rel="icon"]').attr('href', "https://blog.wmz.link/favicon.ico");
          document.title = '哈哈，我就知道！' + OriginTitle;
          titleTime = setTimeout(function () {
              document.title = OriginTitle;
